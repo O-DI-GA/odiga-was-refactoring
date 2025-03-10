cecb2bda2690cd5e2ed523f983a5497eb5e546ca
kimnoca <kimnoca@naver.com>
2025-03-10 17:42:40 +0900

    feat : Owner, Store Entity 생성

---
+++ src/main/java/com/odiga/owner/entity/Owner.java
@@ -0,0 +1,43 @@
+package com.odiga.owner.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.odiga.global.entity.BaseEntity;
+import com.odiga.store.entity.Store;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Owner extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String email;
+
+	private String password;
+
+	private String name;
+
+	@Builder.Default
+	@OneToMany(mappedBy = "owner", cascade = CascadeType.ALL)
+	private List<Store> stores = new ArrayList<>();
+
+	public void addStore(Store store) {
+		stores.add(store);
+		store.setOwner(this);
+	}
+}
---
+++ src/main/java/com/odiga/store/entity/Store.java
@@ -0,0 +1,35 @@
+package com.odiga.store.entity;
+
+import com.odiga.global.entity.BaseEntity;
+import com.odiga.owner.entity.Owner;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Store extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String name;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "OWNER_ID")
+	private Owner owner;
+
+	public void setOwner(Owner owner) {
+		this.owner = owner;
+	}
+}
