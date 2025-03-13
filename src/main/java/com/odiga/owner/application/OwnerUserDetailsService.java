49f50786cbef6b938fef6b62cd47028d8e3afe9b
kimnoca <kimnoca@naver.com>
2025-03-13 19:30:31 +0900

    teat : OwnerUserDetailsService 테스트 코드 작성

--- src/main/java/com/odiga/owner/application/OwnerUserDetailsService.java
+++ src/main/java/com/odiga/owner/application/OwnerUserDetailsService.java
@@ -1,8 +1,8 @@
 package com.odiga.owner.application;
 
 import com.odiga.owner.dao.OwnerRepository;
+import com.odiga.owner.entity.Owner;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -14,7 +14,7 @@ public class OwnerUserDetailsService implements UserDetailsService {
     private final OwnerRepository ownerRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public Owner loadUserByUsername(String username) throws UsernameNotFoundException {
         return ownerRepository.findByEmail(username)
             .orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 email 입니다."));
     }
---
+++ src/test/java/com/odiga/owner/application/OwnerUserDetailsServiceTest.java
@@ -0,0 +1,47 @@
+package com.odiga.owner.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.odiga.common.type.Role;
+import com.odiga.owner.dao.OwnerRepository;
+import com.odiga.owner.entity.Owner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.security.core.GrantedAuthority;
+
+@SpringBootTest(webEnvironment = WebEnvironment.NONE)
+class OwnerUserDetailsServiceTest {
+
+    @Autowired
+    OwnerUserDetailsService ownerUserDetailsService;
+
+    @Autowired
+    OwnerRepository ownerRepository;
+
+    @BeforeEach
+    void init() {
+        Owner owner = Owner.builder()
+            .email("example@google.com")
+            .password("password")
+            .name("name")
+            .build();
+
+        ownerRepository.save(owner);
+    }
+
+    @Test
+    @DisplayName("")
+    void findOwnerByUsernameTest() {
+        Owner owner = ownerUserDetailsService.loadUserByUsername("example@google.com");
+
+        GrantedAuthority grantedAuthority = owner.getAuthorities().stream().findFirst()
+            .orElse(null);
+
+        assertThat(grantedAuthority).isNotNull();
+        assertThat(grantedAuthority.toString()).isEqualTo(Role.ROLE_OWNER.name());
+    }
+}
