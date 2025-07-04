f9590e9da60eb0fc8cb7c229311efcfa8d2f869a
kimnoca <kimnoca@naver.com>
2025-07-04 23:54:13 +0900

    test : 회원가입 중복 email test code 작성

--- src/test/java/com/odiga/user/application/UserAuthServiceTest.java
+++ src/test/java/com/odiga/user/application/UserAuthServiceTest.java
@@ -1,5 +1,6 @@
 package com.odiga.user.application;
 
+import com.odiga.global.exception.CustomException;
 import com.odiga.global.jwt.JwtTokenDto;
 import com.odiga.global.jwt.JwtTokenProvider;
 import com.odiga.owner.entity.Owner;
@@ -8,16 +9,19 @@ import com.odiga.user.dto.UserInfoResponseDto;
 import com.odiga.user.dto.UserLoginRequestDto;
 import com.odiga.user.dto.UserSignupRequestDto;
 import com.odiga.user.entity.User;
+import com.odiga.user.exception.UserErrorCode;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -49,6 +53,19 @@ class UserAuthServiceTest {
         verify(userRepository, times(1)).save(any(User.class));
 
         assertThat(jwtTokenDto.accessToken()).isEqualTo("accessToken");
+    }
+
+    @Test
+    void signupFailTest() {
+        String email = "example@gmail.com";
+
+        when(userRepository.existsByEmail(email)).thenReturn(true);
+
+        UserSignupRequestDto userSignupRequestDto = new UserSignupRequestDto(email, "password", "nickname");
+
+        assertThatThrownBy(() -> userAuthService.signupUser(userSignupRequestDto))
+                .isInstanceOf(CustomException.class)
+                .hasFieldOrPropertyWithValue("errorCode", UserErrorCode.EMAIL_CONFLICT);
     }
 
     @Test
